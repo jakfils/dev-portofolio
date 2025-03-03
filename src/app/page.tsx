56894eefc977391242a45c7094da69e20f751b4d
jakfils <jakfils@gmail.com>
2025-03-03 21:25:15 +0100

    Add Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@/components/Button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <h1 className="text-8xl font-poppins">Bonjour</h1>
+    <>
+      <header className="flex justify-around items-center pt-8 px sticky top-0">
+        <Link href="/">j@kfils</Link>
+        <nav className="flex gap-4">
+          <Button className="button-primary-1">About me</Button>
+          <Button className="button-primary-2">Contact</Button>
+        </nav>
+      </header>
+    </>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+export function Button({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return <button className={className}>{children}</button>;
+}
