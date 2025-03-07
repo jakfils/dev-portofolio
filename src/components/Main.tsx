6fb61ed78b6a08e011db2839b4f165d1110894bc
jakfils <jakfils@gmail.com>
2025-03-07 18:56:23 +0100

    apostrophes in text to follow React best practices

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,12 +7,13 @@ const Main = () => {
       <section className="flex flex-col items-center gap-4">
         <Image src="/me.webp" height={200} width={200} alt="Me" />
         <h1 className="font-poppins text-center text-5xl font-bold">
-          Ibrahim Memon!
+          {" Ibrahim Memon!"}
         </h1>
-        <h3>I do code & play </h3>
+        <h3>{"I do code & play "}</h3>
         <p className="mx-5 text-center text-xs/6 md:mx-30">
-          Passionate Software Engineer with a focus on React Native development,
-          dedicated to crafting elegant and user-friendly mobile applications.
+          {
+            "Passionate Software Engineer with a focus on React Native development, dedicated to crafting elegant and user-friendly mobile applications."
+          }
         </p>
         <Button className="button-primary-3">Contact Me</Button>
       </section>
@@ -21,16 +22,14 @@ const Main = () => {
         <h3 className="text-gradient text-center">EXPLORE NOW</h3>
         <p className="mx-5 text-center text-xs/6 md:mx-30">
           <span className="mb-3 block">
-            As a passionate software engineer, I thrive on theintricate dance
-            between logic and creativity. Currently immersed in the dynamic
-            world of WebHR, my expertise centers around React Native, where I
-            seamlessly blend technology with innovation.
+            {
+              " As a passionate software engineer, I thrive on theintricate dance between logic and creativity. Currently immersed in the dynamic world of WebHR, my expertise centers around React Native, where I seamlessly blend technology with innovation."
+            }
           </span>
           <span>
-            With a fervor for crafting elegant solutions, I navigate the
-            ever-evolving landscape of software development. My journey involves
-            translating concepts into code, creating seamless user experiences,
-            and constantly pushing the boundaries of what's possible
+            {
+              "With a fervor for crafting elegant solutions, I navigate the ever-evolving landscape of software development. My journey involves translating concepts into code, creating seamless user experiences, and constantly pushing the boundaries of what's possible"
+            }
           </span>
         </p>
       </section>
@@ -45,22 +44,14 @@ const Main = () => {
         </div>
         <p className="mx-5 text-center text-xs/6 md:mx-30">
           <span className="mb-3 block">
-            Currently, I am working on WebHR Mobile Application, WebHR is a
-            Cloud based Social HR Software for SMEs by Verge Systems Inc. WebHR
-            is currently used in over 160 countries world wide by thousands of
-            organizations to manage HR, As a React.js developer with 1.5 year of
-            experience, I have a strong foundation in creating dynamic and
-            responsive mobile and web applications.
+            {
+              " Currently, I am working on WebHR Mobile Application, WebHR is a Cloud based Social HR Software for SMEs by Verge Systems Inc. WebHR is currently used in over 160 countries world wide by thousands of organizations to manage HR, As a React.js developer with 1.5 year of experience, I have a strong foundation in creating dynamic and responsive mobile and web applications."
+            }
           </span>
           <span>
-            My experience with React Native has allowed me to develop
-            cross-platform mobile applications that run seamlessly on both iOS
-            and Android platforms. Additionally, my proficiency in React.js has
-            equipped me with the skills to create fast, scalable, and dynamic
-            web pages with excellent user experiences. I have a deep
-            understanding of component-based architecture and state management,
-            and I am well-versed in the latest web development trends and
-            technologies.
+            {
+              "My experience with React Native has allowed me to develop cross-platform mobile applications that run seamlessly on both iOS and Android platforms. Additionally, my proficiency in React.js has equipped me with the skills to create fast, scalable, and dynamic  web pages with excellent user experiences. I have a deep understanding of component-based architecture and state management, and I am well-versed in the latest web development trends and technologies."
+            }
           </span>
         </p>
         <div className="flex flex-row justify-center gap-3 md:w-2/3 md:justify-start">
@@ -81,7 +72,7 @@ const Main = () => {
       <section className="flex flex-col items-center gap-4">
         <h2 className="font-poppins text-5xl">PROJECTS</h2>
         <h3 className="text-gradient text-center">EXPLORE NOW</h3>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 w-4/5">
+        <div className="grid w-4/5 grid-cols-1 gap-4 md:grid-cols-2">
           <Project
             title="WebHR"
             imgUrl="/image-2.webp"
